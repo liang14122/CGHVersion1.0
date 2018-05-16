8f89f40a43c41db15f9507b45e59a37370c0a3dc
liang3336 <632944695@qq.com>
2018-05-16 23:27:34 +0800

    Logo&bg. Got error

--- app/src/main/java/com/example/a16004118/cghversion10/MainActivity.java
+++ app/src/main/java/com/example/a16004118/cghversion10/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.a16004118.cghversion10;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    ImageView ivBackground;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set background to fit parent
+        ivBackground = findViewById(R.id.ivBackground);
+        ivBackground.setScaleType(ImageView.ScaleType.FIT_XY);
     }
 }
