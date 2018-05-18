c4f30e8e536a28b7b8f66d2a1964ad9005bca561
liang3336 <632944695@qq.com>
2018-05-18 10:43:00 +0800

    Login Finished

--- app/src/main/java/com/example/a16004118/cghversion10/MainActivity.java
+++ app/src/main/java/com/example/a16004118/cghversion10/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a16004118.cghversion10;
 
 import android.app.Activity;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.RelativeLayout;
 public class MainActivity extends Activity {
 
     ImageView ivBackground;
+    TextInputLayout tipUsername, tipPassword;
 
 
     @Override
@@ -19,5 +21,11 @@ public class MainActivity extends Activity {
         //set background to fit parent
         ivBackground = findViewById(R.id.ivBackground);
         ivBackground.setScaleType(ImageView.ScaleType.FIT_XY);
+
+        tipPassword = findViewById(R.id.input_layout_passwrod);
+        tipUsername = findViewById(R.id.input_layout_userName);
+
+        tipUsername.setHint("Username/MRC");
+        tipPassword.setHint("Password");
     }
 }
