36b488489afcc78b40721b7cee54b8ab9800a66e
liang3336 <632944695@qq.com>
2018-05-27 18:11:40 +0800

    functions setting

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,40 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+exports.updateMessage = functions.https.onCall((data) => {
+  const text = data.text;
+
+  return admin.database().ref('/message').push({
+    text: data
+  }).then(() => {
+    console.log('New Message written');
+    return { text: data };
+  })
+  .catch((error) => {
+    throw new functions.https.HttpsError('unknown', error.message, error);
+  });
+
+});
+
+exports.sendNotification = functions.database.ref('/message').onWrite(event=>{
+
+var ref = admin.database().ref('/token');
+return ref.once("value", function(snapshot){
+
+    const payload = {
+          notification: {
+            title: 'update message.',
+            body: 'Successfully'
+          }
+        };
+
+        admin.messaging().sendToDevice(snapshot.val(), payload)
+
+        },
+    function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+});
+})
